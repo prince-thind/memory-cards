a9736b0105fbcd6e9c611182c480277dc0319263
Prince Thind <princethind799@gmail.com>
2021-06-29 22:52:39 +0530

    add images

---
+++ src/components/images.js
@@ -0,0 +1,16 @@
+import image1 from '../images/1.jpg';
+import image2 from '../images/2.jpg';
+import image3 from '../images/3.jpg';
+import image4 from '../images/4.jpg';
+import image5 from '../images/5.jpg';
+import image6 from '../images/6.jpg';
+import image7 from '../images/7.jpg';
+import image8 from '../images/8.jpg';
+import image9 from '../images/9.jpg';
+import image10 from '../images/10.jpg';
+import image11 from '../images/11.jpg';
+import image12 from '../images/12.jpg';
+
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11,image12];
+
+export default images;
