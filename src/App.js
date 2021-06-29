e90c146fa8dfae215e1bbe1e567f6247c9574826
Prince Thind <princethind799@gmail.com>
2021-06-29 10:14:59 +0530

    create-react-app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className="heading">Memory Cards</h1>
+        <h2 className="heading-intro">Goal: Click on every card only once</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
