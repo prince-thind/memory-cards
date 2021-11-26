ccbdfb6c5a0939cab6a512ad4e8e6c74a032c19e
Prince Thind <Princethind799@gmail.com>
2021-11-26 17:10:45 +0530

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import { useState, useEffect } from "react";
-import GameBoard from "./components/GameBoard";
-// import images from "./components/images";
-import icons from "./lib/icons";
-import InfoBar from "./components/InfoBar";
 import _ from "loadsh";
 import uniqid from "uniqid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRedo } from "@fortawesome/free-solid-svg-icons";
+
+import GameBoard from "./components/GameBoard";
+import icons from "./lib/icons";
+import InfoBar from "./components/InfoBar";
 
 function App() {
   const [bestScore, setBestScore] = useState(0);
   const [score, setScore] = useState(0);
   const [gameStatus, setGameStatus] = useState("running");
   const [characterArr, setCharacterArr] = useState(initCharacterArr());
-
-  function initCharacterArr() {
-    let arr = [];
-    for (let i = 0; i < icons.length; i++) {
-      arr.push({ icon: icons[i], key: uniqid() });
-    }
-    return arr;
-  }
 
   useEffect(() => {
     if (score > bestScore) {
@@ -31,11 +23,30 @@ function App() {
     }
   }, [score, bestScore, characterArr]);
 
-  // useEffect(() => {
-  //   if (gameStatus === 'lost') {
-  //     setScore(0);
-  //   }
-  // }, [gameStatus]);
+  return (
+    <div className="App">
+      <header>
+        <h1 className="heading">Memory Cards</h1>
+        <h2 className="status-bar bar">
+          BestScore: {bestScore} <div>Score: {score} </div>
+          <button
+            className="reset-button"
+            onClick={() => {
+              gameSub("reset");
+            }}
+          >
+            <FontAwesomeIcon icon={faRedo} />
+          </button>
+        </h2>
+        <InfoBar gameStatus={gameStatus} setGameStatus={setGameStatus} />
+      </header>
+      <GameBoard
+        characterArr={characterArr}
+        gameStatus={gameStatus}
+        gameSub={gameSub}
+      />
+    </div>
+  );
 
   function gameSub(signal) {
     if (signal === "lost") {
@@ -58,30 +69,13 @@ function App() {
     }
   }
 
-  return (
-    <div className="App">
-      <header>
-        <h1 className="heading">Memory Cards</h1>
-        <h2 className="status-bar bar">
-         BestScore: {bestScore} <div>Score: {score}  </div>
-          <button
-            className="reset-button"
-            onClick={() => {
-              gameSub("reset");
-            }}
-          >
-           <FontAwesomeIcon icon={faRedo}/>
-          </button>
-        </h2>
-        <InfoBar gameStatus={gameStatus} setGameStatus={setGameStatus} />
-      </header>
-      <GameBoard
-        characterArr={characterArr}
-        gameStatus={gameStatus}
-        gameSub={gameSub}
-      />
-    </div>
-  );
+  function initCharacterArr() {
+    let arr = [];
+    for (let i = 0; i < icons.length; i++) {
+      arr.push({ icon: icons[i], key: uniqid() });
+    }
+    return arr;
+  }
 }
 
 export default App;
